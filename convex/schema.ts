88dd248afc7efac71a259bec02c00d5da4821858
GokouWS <lynxyyeu@gmail.com>
2025-03-06 16:41:26 +0000

    Clerk convex webhook setup

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,12 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
-const schema = defineSchema({});
+const schema = defineSchema({
+  users: defineTable({
+    name: v.string(),
+    // this the Clerk ID, stored in the subject JWT field
+    externalId: v.string(),
+  }).index("byExternalId", ["externalId"]),
+});
 
 export default schema;

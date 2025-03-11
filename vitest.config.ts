fc229efa1c3d1330745530fc2b13250a6fafe9ef
GokouWS <lynxyyeu@gmail.com>
2025-03-11 13:18:31 +0000

    tsconfigPaths added to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  plugins: [
+    tsconfigPaths({
+      projects: ["./tsconfig.json"],
+      root: "./",
+    }) as any,
+  ],
   test: {
     globals: true,
     environment: "jsdom",
